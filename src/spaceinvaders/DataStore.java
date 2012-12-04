4b388e8b9d1a3eaede13fbc5ee0b6b5673dbb0b8
heinrich10 <heinrich10@localhost>
2012-12-05 01:46:58 +0800

    final beta

---
+++ src/spaceinvaders/DataStore.java
@@ -0,0 +1,26 @@
+package spaceinvaders;
+
+public class DataStore {
+	private int x;
+	private int y;
+	
+	public DataStore(){
+		
+	}
+	
+	public void setX(int x){
+		this.x = x;
+	}
+	
+	public void setY(int y){
+		this.y = y;
+	}
+	
+	public int getY(){
+		return y;
+	}
+	
+	public int getX(){
+		return x;
+	}
+}
---
+++ src/spaceinvaders/StoryHelper.java
@@ -0,0 +1,14 @@
+package spaceinvaders;
+
+public class StoryHelper {
+	public String stageOne(){
+		return "stage 1";
+	}
+	
+	public String stageTwo(){
+		return "stage 2";
+	}
+	public String stageThree(){
+		return "stage 3";
+	}
+}
